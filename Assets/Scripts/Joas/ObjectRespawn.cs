960f83ba7422936151424098cfd97a5f012b4c2a
Joas Sahetapy <joassahetapy@gmail.com>
2023-02-03 23:50:38 +0100

    RespawnObject Version 1

--- Assets/Scripts/Joas/ObjectRespawn.cs
+++ Assets/Scripts/Joas/ObjectRespawn.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ObjectRespawn : MonoBehaviour
 {
+    public GameObject objectToRespawn;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //make the object respawn after falling under y position -10
+        if (objectToRespawn.transform.position.y < -10)
+        {
+            objectToRespawn.transform.position = new Vector3(0, 0, -55);
+            Debug.Log("Respawned");
+        }
         
     }
 }
